2d12239f713b75a74e0732496306e2f2211b4748
varmapalleti <nageshvarmapalleti@gmail.com>
2026-07-19 16:01:59 +0530

    created directories

---
+++ events-booking/src/test/java/com/collabs/events_booking/EventsBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabs.events_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
